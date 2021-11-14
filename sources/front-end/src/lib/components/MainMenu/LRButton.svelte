68ff3b0e3a938f45b4e6d9c3200eb9349fd6d00a
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-14 21:33:17 +0100

    added some interactivity to the main menu

--- sources/front-end/src/lib/components/MainMenu/LRButton.svelte
+++ sources/front-end/src/lib/components/MainMenu/LRButton.svelte
@@ -1,21 +1,34 @@
 <style>
   div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     align-self: center;
     justify-self: center;
 
-    width: 0.75vw;
-    height: 0.75vw;
+    width: 100%;
+    height: 100%;
 
-    border-radius: 50%;
+    font-size: max(3vw, 3vh);
+    color: hsl(25deg 100% 50% / 100%);
+    filter: grayscale(1.0);
 
     cursor: pointer;
-
-    background-color: hsl(0deg 0% 43%);
+    transition: all 0.5s var(--transition-timing-function) 0s;
   }
 
   div:hover {
-    background-color: hsl(0deg 0% 63%);
+    filter: grayscale(0.25);
+    transition: all var(--transition-duration) var(--transition-timing-function) 0s;
+  }
+
+  div:active {
+    filter: grayscale(0.0);
+    transition: all var(--transition-duration) var(--transition-timing-function) 0s;
   }
 </style>
 
-<div></div>
+<div>
+  <slot></slot>
+</div>
