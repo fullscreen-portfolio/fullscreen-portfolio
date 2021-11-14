785fcd44e4b0d5bd13f63c21fce15d9290f60682
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-14 16:12:46 +0100

    added MainMenu + LRButton

---
+++ sources/front-end/src/lib/components/MainMenu/LRButton.svelte
@@ -0,0 +1,21 @@
+<style>
+  div {
+    align-self: center;
+    justify-self: center;
+
+    width: 0.75vw;
+    height: 0.75vw;
+
+    border-radius: 50%;
+
+    cursor: pointer;
+
+    background-color: hsl(0deg 0% 43%);
+  }
+
+  div:hover {
+    background-color: hsl(0deg 0% 63%);
+  }
+</style>
+
+<div></div>
---
+++ sources/front-end/src/lib/components/MainMenu/MainMenu.svelte
@@ -0,0 +1,60 @@
+<script>
+  import LRButton from './LRButton.svelte';
+</script>
+
+<style>
+  #main-menu-control {
+    display: grid;
+    grid-template-columns: 0.5fr 1fr 0.5fr;
+    grid-template-rows: 0.5fr 1fr 0.5fr;
+    grid-template-areas:
+      '. . .'
+      'L C R'
+      '. . .'
+    ;
+
+    background-color: hsl(0deg 0% 5% / 42%);
+    width: 8rem;
+    height: 8rem;
+    border-radius: 50%;
+    position: relative;
+  }
+
+  #move-left,
+  #move-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  #move-left {
+    grid-area: L;
+    align-items: flex-start;
+  }
+
+  #move-right {
+    grid-area: R;
+    align-items: flex-end;
+  }
+
+  #center {
+    grid-area: C;
+    background-color: red;
+    width: 90%;
+    height: 90%;
+    justify-self: center;
+    align-self: center;
+
+    border-radius: 50%;
+  }
+</style>
+
+<div id='main-menu-control'>
+  <div id='move-left'>
+    <LRButton />
+  </div>
+  <div id='center'>&nbsp;</div>
+  <div id='move-right'>
+    <LRButton />
+  </div>
+</div>
