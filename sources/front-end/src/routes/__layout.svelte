22bf6d4964a3805216f50e65f4f4bb3561c650d4
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-14 15:41:30 +0100

    tweaked layout and style

--- sources/front-end/src/routes/__layout.svelte
+++ sources/front-end/src/routes/__layout.svelte
@@ -1,3 +1,19 @@
 <script>
   import '../app.css';
 </script>
+
+<style>
+  article {
+    display: flex;
+    flex-direction: column;
+    flex: 1 0 auto;
+    justify-content: flex-start;
+    align-items: center;
+
+    min-width: 90vw;
+  }
+</style>
+
+<article>
+
+</article>
