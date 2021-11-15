92809341c5461c3d72303e3fad10e1de8da2b212
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-15 13:38:57 +0100

    added to main menu + nav

---
+++ sources/front-end/src/lib/components/MainMenu/NavMenuControl.svelte
@@ -0,0 +1,51 @@
+<script>
+  import HomeIcon from '$lib/icons/home.svelte';
+  import ServicesIcon from '$lib/icons/aperture.svelte';
+  import MailIcon from '$lib/icons/mail.svelte';
+import { page } from '../../../../.svelte-kit/dev/runtime/app/stores';
+</script>
+
+<style>
+  #menu-container {
+    display: flex;
+
+    min-height: 8rem;
+    background-color: hsl(39deg 72% 55%);
+
+    border-radius: var(--border-radius);
+  }
+
+  .menu-item {
+    display: flex;
+    flex-direction: column;
+    flex: 1 0 auto;
+
+    justify-content: space-evenly;
+    align-items: center;
+
+    border: none;
+    background-color: transparent;
+    color: hsl(0deg 0% 100%);
+
+    cursor: pointer;
+
+    border-radius: var(--border-radius);
+  }
+
+  .active {
+    color: red;
+    background-color: yellow;
+  }
+</style>
+
+<div id='menu-container'>
+  <a sveltekit:prefetch href='/' rel='noreferrer' class='menu-item' class:active={$page.path === '/'}>
+    <HomeIcon />
+  </a>
+  <a sveltekit:prefetch href='/services' rel='noreferrer' class='menu-item' class:active={$page.path === '/services'}>
+    <ServicesIcon />
+  </a>
+  <a sveltekit:prefetch href='/contact-us' rel='noreferrer' class='menu-item' class:active={$page.path === '/contact-us'}>
+    <MailIcon />
+  </a>
+</div>
--- sources/front-end/svelte.config.js
+++ sources/front-end/svelte.config.js
@@ -1,7 +1,20 @@
+import adapterNode from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  compilerOptions: {
+    format: 'esm',
+    hydratable: true,
+  },
+  extensions: ['.svelte'],
   kit: {
+    adapter: adapterNode({
+      precompress: true,
+    }),
     target: '#portfolio',
+    hydrate: true,
+    router: true,
+    ssr: true,
   }
 };
 
