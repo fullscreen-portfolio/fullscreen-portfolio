87f945111bf736fa12c70ac32191fde0fc831fb6
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-11-18 13:14:47 +0100

    WIP: images

---
+++ sources/front-end/src/lib/components/Gallery/Gallery.svelte
@@ -0,0 +1,29 @@
+<style>
+  article {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    justify-self: center;
+    align-self: center;
+
+    max-width: min(95vh, 95vw);
+    max-height: min(95vh, 95vw);
+  }
+
+  img {
+    aspect-ratio: 1 / 1;
+    object-position: 50% 50%;
+    object-fit: contain;
+
+    max-width: min(95vh, 95vw);
+    max-height: min(95vh, 95vw);
+  }
+</style>
+
+<article>
+  <picture>
+    <source srcset="/images/DSC_0132.jpg" media="(max-height: 2040px)">
+    <img srcset="/images/DSC_0132.jpg" alt="Shopify Merchant, Corrine Anestopoulos">
+  </picture>
+</article>
